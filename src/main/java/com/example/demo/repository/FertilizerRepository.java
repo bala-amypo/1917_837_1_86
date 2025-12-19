993236b3244eea0f9677eeb73c2050d189fb0050
bala-amypo <bala@amypo.in>
2025-12-19 07:54:02 +0000

    User commit

--- src/main/java/com/example/demo/repository/FertilizerRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.entity.Fertilizer;
-
-public interface FertilizerRepository extends JpaRepository<Fertilizer, Long> {
-}
