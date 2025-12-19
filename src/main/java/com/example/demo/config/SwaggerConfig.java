b48dffdc89011a9e77bcbc8636f96bd8d884fecf
bala-amypo <bala@amypo.in>
2025-12-19 09:45:02 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -1,28 +1,20 @@
 package com.example.demo.config;
 
-import io.swagger.v3.oas.models.*;
-import io.swagger.v3.oas.models.security.*;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
 
     @Bean
-    public OpenAPI openAPI() {
-
-        SecurityScheme scheme = new SecurityScheme()
-                .type(SecurityScheme.Type.HTTP)
-                .scheme("bearer")
-                .bearerFormat("JWT");
-
+    public OpenAPI customOpenAPI() {
         return new OpenAPI()
-                .info(new Info()
-                        .title("Crop & Fertilizer Suggestion API")
-                        .version("1.0"))
-                .components(new Components()
-                        .addSecuritySchemes("bearerAuth", scheme))
-                .addSecurityItem(new SecurityRequirement()
-                        .addList("bearerAuth"));
-    }
+                // You need to change the port as per your server
+                .servers(List.of(
+                        new Server().url("https://9406.pro604cr.amypo.ai/")
+                ));
+        }
 }
